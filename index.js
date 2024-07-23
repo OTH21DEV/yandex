2af3c549a69ce486e59cec5d038a7487049eae09
Oxana Theis <otheis@protonmail.com>
2024-07-23 11:10:03 +0200

    add marquee and marquee animation

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 import { fetchParticipants } from "./scripts/participants/fetchParticipants.js";
 import { setParticipants, showPage, startAutoSlide } from "./scripts/participants/pagination.js";
 import { fetchStages } from "./scripts/stages/fetchStages.js";
-import { renderDots,showPageStage,setStages } from "./scripts/stages/pagination.js";
+import { renderDots, showPageStage, setStages } from "./scripts/stages/pagination.js";
 import { renderStages } from "./scripts/stages/renderStages.js";
-
+import { marqueeMeasure, marqueeKeyframes } from "./scripts/marquee.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   try {
     const participantsData = await fetchParticipants();
-    const stagesData = await fetchStages()
+    const stagesData = await fetchStages();
     setParticipants(participantsData);
-  
+
     showPage(1);
     startAutoSlide();
     //
-    setStages(stagesData)
-   showPageStage(1)
-     renderDots(stagesData.length)
-renderStages(stagesData)
-
+    setStages(stagesData);
+    showPageStage(1);
+    renderDots(stagesData.length);
+    renderStages(stagesData);
+    // marquee measure and keyframe for infinite loop of 3 phrases
+    const measuredWidth = marqueeMeasure();
+    marqueeKeyframes(measuredWidth);
   } catch (error) {
     console.error("Error fetching", error);
   }
---
+++ scripts/marquee.js
@@ -0,0 +1,42 @@
+/**
+ * Measures and sets the width of
+ * the marquee based on the first three phrases
+ * @returns {number} phraseWidth - the calculated width of the marquee (3 phrases) in pixels.
+ */
+export function marqueeMeasure() {
+  const marqueePhrases = document.querySelectorAll(".marquee__phrase");
+  let phraseWidth = 0;
+  for (let i = 0; i < 3; i++) {
+    phraseWidth += marqueePhrases[i].offsetWidth;
+  }
+
+  document.querySelector(".marquee").style.width = `${phraseWidth}px`;
+
+  return phraseWidth;
+}
+/**
+ * Generates and applies keyframe animation to
+ * scroll marquee content based on its width
+ * @param {number} phraseWidth the calculated width of the marquee (3 phrases) in pixels
+ */
+export function marqueeKeyframes(phraseWidth) {
+  const marqueeContent = document.querySelector(".marquee__content");
+  marqueeContent.style.transform = `translateX(0)`;
+
+  const keyframes = `
+      @keyframes ticker {
+        from {
+          transform: translateX(0);
+        }
+        to {
+          transform: translateX(-${phraseWidth}px);
+        }
+      }
+    `;
+  const styleSheet = document.styleSheets[0];
+  styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
+
+  const speed = 50; // px/s
+  const duration = phraseWidth / speed;
+  marqueeContent.style.animation = `ticker ${duration}s linear infinite`;
+}
