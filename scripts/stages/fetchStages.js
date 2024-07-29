1f068a6acc3d496a9a31ffa8299e58afea915e8d
Oxana Theis <otheis@protonmail.com>
2024-07-29 11:31:51 +0200

    adjust marquee

--- scripts/stages/fetchStages.js
+++ scripts/stages/fetchStages.js
@@ -1,7 +1,6 @@
-import { fetchData } from "../fetchData.js"
-import { BASE_URL } from "../../config.js"
+import { fetchData } from "../fetchData.js";
+import { BASE_URL } from "../../config.js";
 
-export async function fetchStages(){
-//  return await fetchData("../../data/stages.json")
-return await fetchData(`${BASE_URL}/data/stages.json`)
+export async function fetchStages() {
+  return await fetchData(`${BASE_URL}/data/stages.json`);
 }
