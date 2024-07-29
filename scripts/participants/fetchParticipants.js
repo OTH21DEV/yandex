77974a59c5c628412fd152208be5d19e34998300
Oxana Theis <otheis@protonmail.com>
2024-07-29 11:14:16 +0200

    adjuct config.js

--- scripts/participants/fetchParticipants.js
+++ scripts/participants/fetchParticipants.js
@@ -3,5 +3,5 @@ import { BASE_URL } from "../../config.js";
 
 export async function fetchParticipants(){
   // return await fetchData("../../data/participants.json")
-  return await fetchData(`${BASE_URL/data/participants.json}`)
+  return await fetchData(`${BASE_URL}/data/participants.json`)
 }
--- scripts/stages/fetchStages.js
+++ scripts/stages/fetchStages.js
@@ -3,5 +3,5 @@ import { BASE_URL } from "../../config.js"
 
 export async function fetchStages(){
 //  return await fetchData("../../data/stages.json")
-return await fetchData(`${BASE_URL/data/stages.json}`)
+return await fetchData(`${BASE_URL}/data/stages.json`)
 }
